6663526994da2b05b402e663804cf1cbf9bb4157
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-06-02 12:37:13 -0400

    Final Project 1

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Illia
+ * Created on: Sep 2026
+ * This program 
 */
 
-basic.showString('Hello, World!')
+let distance = 0
+
+basic.forever(function () {
+   
+    distance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    if (distance > 0 && distance < 10) {
+
+       
+        pins.digitalWritePin(DigitalPin.P8, 1) 
+       
+    } else {
+
+   
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        basic.clearScreen()
+
+    }
+
+    basic.pause(100)
+})
