6e96e57a6eaad6b8f0c9de379cb1ed269146cecc
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-06-08 13:21:00 -0400

    Final Project 7

--- main.ts
+++ main.ts
@@ -5,11 +5,12 @@
  * This program Automatic light switching
 */
 
+// variables
 let distance = 0
-
 let servoDirection = 0
 let motionDetected = false
 
+// setup of Sonar
 basic.forever(function () {
    
     distance = sonar.ping(
@@ -18,19 +19,24 @@ basic.forever(function () {
         PingUnit.Centimeters
     )
 
+    //sensor response range
     if (distance > 0 && distance < 10) {
-        
+     
+     //when the sensor was triggered
      if (!motionDetected) {
       motionDetected = true
-
+        
+        //turning on the LED
         pins.digitalWritePin(DigitalPin.P8, 1)
             
+        //Servo positions
         if (servoDirection == 0) {
                 
+                //change the servo position to 0 degrees
                 robotbit.Servo(robotbit.Servos.S8, 0)
                 servoDirection = 1
         } else {
-                
+                //change the servo position to 180 degrees
                 robotbit.Servo(robotbit.Servos.S8, 180)
                 servoDirection = 0
         }
@@ -39,7 +45,7 @@ basic.forever(function () {
         }
 
     } else {
-        
+        //turning off the LED
         motionDetected = false
         pins.digitalWritePin(DigitalPin.P8, 0)
         basic.clearScreen()
